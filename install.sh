406f38b252136a46705d79de1d39370e48ec0e4f
Anashi Kitsun <diamondguy9700@gmail.com>
2021-02-12 22:16:48 -0700

    fixed the link directory

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ mkdir ~/.oh-my-zsh/custom/themes/omz-themes/ > /dev/null 2>&1
 echo "Created custom subfolder"
 rm -f ~/.oh-my-zsh/custom/themes/omz-themes/*
 echo "Cleaned custom subfolder"
-ln -s ~/omz-themes/themes/* ~/.oh-my-zsh/custom/themes/omz-themes/
+ln -s ~/github/omz-themes/themes/* ~/.oh-my-zsh/custom/themes/omz-themes/
 echo "Shell themes linked!"
